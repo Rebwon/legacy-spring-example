c6568fb4e933b0d15c7ce8d9499281f0964e0464
Rebwon <msolo021015@naver.com>
2020-09-19 10:54:40 +0900

    spring webmvc configuration setting

--- src/main/java/com/rebwon/spring/config/WebConfig.java
+++ src/main/java/com/rebwon/spring/config/WebConfig.java
@@ -1,27 +1,40 @@
 package com.rebwon.spring.config;
 
-import org.springframework.context.annotation.Bean;
+import java.time.Duration;
+import java.util.List;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+import org.springframework.http.CacheControl;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @ComponentScan({"com.rebwon.spring.web"})
+@EnableWebMvc
 @Configuration
-public class WebConfig {
-  @Bean
-  public FreeMarkerViewResolver freemarkerViewResolver() {
-    FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
-    viewResolver.setContentType("text/html; charset=UTF-8");
-    viewResolver.setCache(false);
-    viewResolver.setSuffix(".html");
-    return viewResolver;
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/images/**", "/css/**", "/js/**")
+        .addResourceLocations("classpath:/static/images/", "classpath:/static/css/", "classpath:/static/js/")
+        .setCacheControl(CacheControl.maxAge(Duration.ofDays(1)));
   }
 
-  @Bean
-  public FreeMarkerConfigurer freemarkerConfig() {
-    FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-    freeMarkerConfigurer.setTemplateLoaderPath("classpath:/templates/");
-    return freeMarkerConfigurer;
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    Jackson2ObjectMapperBuilder objectMapperBuilder = new Jackson2ObjectMapperBuilder()
+        .indentOutput(true);
+    converters.add(new MappingJackson2HttpMessageConverter(objectMapperBuilder.build()));
+  }
+
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+    registry.enableContentNegotiation(new MappingJackson2JsonView());
   }
 }
--- src/main/java/com/rebwon/spring/web/HomeController.java
+++ src/main/java/com/rebwon/spring/web/HomeController.java
@@ -1,8 +1,12 @@
 package com.rebwon.spring.web;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -10,5 +14,13 @@ public class HomeController {
   public String home(Model model) throws Exception {
     model.addAttribute("message", "Hello Rebwon!");
     return "index";
+  }
+
+  @GetMapping("/json")
+  @ResponseBody
+  public Map<String, Object> json() throws IOException {
+    Map<String, Object> body = new HashMap<>();
+    body.put("message", "Hello Json!");
+    return body;
   }
 }
